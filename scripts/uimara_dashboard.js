ddad2dcfffa249253df07fe03de16d83e4eed38f
Chukwuemeka Nwagu <developer.nwagu@gmail.com>
2021-04-16 13:30:56 +0100

    add dashboard files

--- scripts/uimara_dashboard.js
+++ scripts/uimara_dashboard.js
@@ -4,6 +4,8 @@ async function loadWeb3() {
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     window.ethereum.enable();
+  } else {
+	  console.log(window.ethereum)
   }
 }
 
