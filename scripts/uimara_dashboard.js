91801b5e84aeb84d152329cf51383dfc00fb0ab8
Chukwuemeka Nwagu <developer.nwagu@gmail.com>
2021-04-16 14:06:35 +0100

    metamask integration

--- scripts/uimara_dashboard.js
+++ scripts/uimara_dashboard.js
@@ -4,8 +4,6 @@ async function loadWeb3() {
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     window.ethereum.enable();
-  } else {
-	  console.log(window.ethereum)
   }
 }
 
